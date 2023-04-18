28e7a416e6dfa995296d2642d524b601800e1c14
Shah Newaz Khan <info@shahnewazkhan.ca>
2023-04-18 10:52:47 -0700

    added interaction retriever run details

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # zapper-tha
 Zapper take home assessment
+
+## Run interaction retriever
+
+The interaction retriever is a python application that, given a smart contract address on the ethereum chain, returns the top 10 addresses that has the most amount of interactions with the address. 
+
+The [_get_interaction_query](interaction_retriever.py#L15) function string interpolates the contract address we want to retrieve the interactions for using the following SQL query template. The interpolated SQL query selects data from the Ethereum network dataset on Google BigQuery to determine the top 10 Ethereum addresses that have had the most interactions with a given contract address in the last month.
+
+It does so by first creating a common table expression (CTE) named [transaction_addresses](interaction_retriever.py#L24), which identifies all the addresses that have interacted with the specified contract address in the last month. To achieve this, the CTE selects all transactions that have occurred in the last month involving the specified contract address, and then extracts the from_address and to_address fields from each transaction using the UNNEST() function. If the to_address of the transaction matches the specified contract address, the from_address is selected as the address, otherwise, the to_address is selected as the address. The resulting CTE contains a list of unique addresses that have interacted with the specified contract address in the last month.
+
+The [main query](interaction_retriever.py#L36) then selects data from the [transaction_addresses](interaction_retriever.py#L24) CTE and groups the data by address. It then counts the number of interactions each address had with the specified contract address and orders the results in descending order of interaction count. Finally, it limits the output to the top 10 addresses with the highest interaction count.
+
+To run the `interaction retriever` app, you can pass in the contract address you want to get interaction data for as the first argument to the python invocation (**note** it [defaults](interaction_retriever.py#L51) to the [USDC](https://etherscan.io/token/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48) contact address ). 
+
+```sh
+
+# Create and activate  virtulaenv and install dependencies
+virtualenv venv -p python3
+source venv/bin/activate 
+pip install -r requirements.txt
+
+# Set GCP Service account envar
+GCP_SA=zapper_tha_sa.json
+
+# Retrieve all interactions with USDT
+python interaction_retriever.py 0xdac17f958d2ee523a2206206994597c13d831ec7
+```
--- interaction_retriever.py
+++ interaction_retriever.py
@@ -9,10 +9,11 @@ logging.basicConfig(level=logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+ZAPPER_THA_SA = os.environ.get('GCP_SA')
 # Setup BQ SA
 BQ = bigquery.Client.from_service_account_json('zapper_tha_sa.json')
 
-def _get_interaction_query(contract_address: str) -> str:
+def _get_interaction_query(CONTRACT_ADDRESS: str) -> str:
     """
     String interpolates a contract address to retrieve interaction
     information from the bigquery-public-data.crypto_ethereum.transactions
@@ -24,13 +25,13 @@ def _get_interaction_query(contract_address: str) -> str:
     WITH transaction_addresses AS (
         SELECT 
             CASE 
-                WHEN to_address = '{contract_address}' THEN from_address 
+                WHEN to_address = '{CONTRACT_ADDRESS}' THEN from_address 
                 ELSE to_address 
             END AS address
         FROM `bigquery-public-data.crypto_ethereum.transactions`,
             UNNEST([to_address, from_address]) AS address
         WHERE DATE(block_timestamp) >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)
-            AND '{contract_address}' IN UNNEST([to_address, from_address])
+            AND '{CONTRACT_ADDRESS}' IN UNNEST([to_address, from_address])
     )
 
     SELECT 
@@ -46,13 +47,13 @@ def _get_interaction_query(contract_address: str) -> str:
 
 # Get the contract address from the command-line argument
 if len(sys.argv) > 1:
-    contract_address = sys.argv[1]
+    CONTRACT_ADDRESS = sys.argv[1]
 else:
-    contract_address = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
+    CONTRACT_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
 
-logger.info(f'Extracting information for contract address: {contract_address}')
+logger.info(f'Extracting information for contract address: {CONTRACT_ADDRESS}')
 
-query = _get_interaction_query(contract_address)
+query = _get_interaction_query(CONTRACT_ADDRESS)
 query_job = BQ.query(query)
 
 # Convert the results to a Pandas DataFrame
@@ -62,14 +63,14 @@ df = query_job.to_dataframe()
 START_DATE = (datetime.now() - timedelta(weeks=4)).strftime("%Y-%m-%d")
 END_DATE = datetime.now().strftime("%Y-%m-%d")
 
-df['_contract_address'] = contract_address
+df['_CONTRACT_ADDRESS'] = CONTRACT_ADDRESS
 df['_start_date'] = START_DATE
 df['_end_date'] =  END_DATE
 
 parquet_path = f'data/interactions'
 if not os.path.exists(parquet_path):
     os.makedirs(parquet_path)
-df.to_parquet(os.path.join(parquet_path, f'{contract_address}.parquet'))
+df.to_parquet(os.path.join(parquet_path, f'{CONTRACT_ADDRESS}_{END_DATE}.parquet'))
 logger.info(f'flushed dataframe to {parquet_path}')
 
 # Print the DataFrame
